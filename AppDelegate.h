e578b0f51cc6aa7ad78fb195f2ed42f0d9781a2d
xiaoyifan <xiaoyifan@uchicago.edu>
2015-11-20 15:08:33 -0600

    First commit to repo

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SharkFeed
+//
+//  Created by Yifan Xiao on 11/20/15.
+//  Copyright © 2015 Yifan Xiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SharkFeed
+//
+//  Created by Yifan Xiao on 11/20/15.
+//  Copyright © 2015 Yifan Xiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
